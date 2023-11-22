ca6e3cd484504ad824007de9b6102b9cde85bc39
jensen-lloyd <jl.110@protonmail.com>
2023-11-22 14:48:54 +1100

    Worked on spacing, ensuring everything stayed in bounds, and looked good on both movile and desktop platforms

--- lib/main.dart
+++ lib/main.dart
@@ -14,46 +14,50 @@ class Home extends StatelessWidget
     Widget build(BuildContext context)
     {
         return Scaffold(
-                appBar: AppBar(  
-                    title: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                            Padding(
-                                padding: EdgeInsets.only(left: 10),
-                                child: Text(
-                                       'X',
-                                       style: TextStyle(
-                                           fontWeight: FontWeight.bold,
-                                           fontStyle: FontStyle.italic,
-                                           fontSize: 40,
+               appBar: PreferredSize(
+                   preferredSize: Size.fromHeight(40),
+               
+                   child: AppBar(  
+                        title: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                                Padding(
+                                    padding: EdgeInsets.only(left: 10),
+                                    child: Text(
+                                           'X',
+                                           style: TextStyle(
+                                               fontWeight: FontWeight.bold,
+                                               fontStyle: FontStyle.italic,
+                                               fontSize: 40,
+                                               ),
                                            ),
-                                       ),
-                                ),
-
-                            Padding(
-                                padding: EdgeInsets.only(left: 30),
-                                child: Text(
-                                       'Platform',
-                                       style: TextStyle(
-                                           fontSize: 32,
-                                           ),
-                                       ),
-                            ),
-
-
-                            Padding(
-                                padding: EdgeInsets.only(left: 5),
-                                child: Icon(
-                                    Icons.arrow_drop_down,
-                                    size: 25,
-                                    color: Colors.lightBlue[100],
                                     ),
-                                ),
-                        ],
-                    ),
 
-                    centerTitle: false, 
-                    backgroundColor: Colors.lightBlue[800], 
+                                Padding(
+                                    padding: EdgeInsets.only(left: 30),
+                                    child: Text(
+                                           'Platform',
+                                           style: TextStyle(
+                                               fontSize: 32,
+                                               ),
+                                           ),
+                                ),
+
+
+                                Padding(
+                                    padding: EdgeInsets.only(left: 5),
+                                    child: Icon(
+                                        Icons.arrow_drop_down,
+                                        size: 25,
+                                        color: Colors.lightBlue[100],
+                                        ),
+                                    ),
+                            ],
+                        ),
+
+                        centerTitle: false, 
+                        backgroundColor: Colors.lightBlue[800], 
+                    ),
                 ),
 
 
@@ -70,12 +74,12 @@ class Home extends StatelessWidget
 			                color: Colors.grey[400],
 			                height: 80,
 			                child: Row(
-				                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+				                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 				                children: 
 				                [
 				                    //Avatar
 					                Container(
-				                        padding: EdgeInsets.fromLTRB(5, 10, 0, 10),
+				                        padding: EdgeInsets.fromLTRB(0, 11, 0, 11),
 						                margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
 						                child: CircleAvatar(
 							                radius: 30,
@@ -83,43 +87,56 @@ class Home extends StatelessWidget
 				                        ),
 				                    ),
 					                
-					                //Name
-					                Container(
-				                        padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
-						                margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-						                width: (MediaQuery.of(context).size.width)*0.4,
-						                child: Text(
-						                    'Jensen Lloyd',
-						                    style: TextStyle(
-						                        fontWeight: FontWeight.w500,
-						                        fontSize: 20,
-						                        color: Colors. black,
-						                    ),
-						                    softWrap: true,
-					                        overflow: TextOverflow.ellipsis,
-					                    ),
+					                
+					                //Name & message preview 
+					                Column(
+					                    children: 
+					                    [
+					                    //Name
+					                    Container(
+				                            padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+						                    margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+						                    width: (MediaQuery.of(context).size.width)*0.7,
+						                    child: Text(
+						                        'Jensen Lloydfgdfgfdgdgdfgfgfdgfdgdfgfdgfdgfdgfdgfdgdfgfd',
+						                        style: TextStyle(
+						                            fontWeight: FontWeight.w500,
+						                            fontSize: 20,
+						                            color: Colors. black,
+						                        ),
+						                        softWrap: true,
+					                            overflow: TextOverflow.ellipsis,
+					                        ),
+				                        ),
+				                        
+				                        //Message preview
+				                        Container(
+				                            padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+						                    margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+						                    width: (MediaQuery.of(context).size.width)*0.7,
+						                    
+						                    //height: (MediaQuery.of(context).size.height)-(MediaQuery.of(context).size.width)*0.125,
+						                    height: 40,
+						                    
+						                    
+					                        child: Text(
+					                            'Message preview blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ',
+					                            style: TextStyle(
+					                                fontWeight: FontWeight.w400,
+					                                fontSize: 15,
+					                                color: Colors. black,
+					                            ),
+					                            softWrap: true,
+					                            overflow: TextOverflow.clip,
+					                        ),
+				                        ),
+				                        ],
 				                    ),
 				                    
-				                    //Message preview
-				                    Container(
-				                        padding: EdgeInsets.fromLTRB(5, 5, 0, 5),
-						                margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-						                width: (MediaQuery.of(context).size.width)*0.35,
-					                    child: Text(
-					                        'Message preview blah blah blah blah blah blah blah blah blah blah ',
-					                        style: TextStyle(
-					                            fontWeight: FontWeight.w400,
-					                            fontSize: 15,
-					                            color: Colors. black,
-					                        ),
-					                        softWrap: true,
-					                        overflow: TextOverflow.clip,
-					                    ),
-				                    ),
 				                    
 				                    //Message age
 				                    Container(
-				                        padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+				                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
 						                margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
 						                child: Text(
 						                    '8m',
