427a82161b01d1b2aab10594e2213a8a46d5da25
jensen-lloyd <jl.110@protonmail.com>
2023-12-07 23:43:03 +1100

    removed appbar file and placed it into individual pages instead

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -5,7 +5,50 @@ import 'AppDrawer.dart';
 import 'CustomAppBar.dart';
 
 
-
+class HomeAppBar extends StatelessWidget
+{
+    @override
+    Widget build(BuildContext context)
+    {
+        return AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 0),
+                child: Text(    
+                  '(logo here)',   
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,  
+                    //fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 65),
+                child: Text(
+                  'SMS',
+                  style: TextStyle(
+                    fontSize: 26,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 0),
+                child: Icon(
+                  Icons.arrow_drop_down,
+                  size: 20,
+                  color: Colors.lightBlue[100],
+                ),
+              ),
+            ],
+          ),
+          centerTitle: false,
+          backgroundColor: Colors.lightBlue[800],
+        );
+    }
+}
 
 
 class HomePage extends StatelessWidget {
@@ -16,7 +59,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50),
-        child: CustomAppBar(),
+        child: HomeAppBar(),
         ),
         
       drawer: AppDrawer(),  
--- lib/MessagesPage.dart
+++ lib/MessagesPage.dart
@@ -2,10 +2,54 @@ import 'package:flutter/material.dart';
 import 'functions.dart';
 
 import 'AppDrawer.dart';
-import 'AppBar.dart';
 
 
 
+
+class MessageAppBar extends StatelessWidget
+{
+    @override
+    Widget build(BuildContext context)
+    {
+        return AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(left: 0),
+                child: Text(
+                  '(logo here)',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    //fontStyle: FontStyle.italic,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 65),
+                child: Text(
+                  'SMS',
+                  style: TextStyle(
+                    fontSize: 26,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 0),
+                child: Icon(
+                  Icons.arrow_drop_down,
+                  size: 20,
+                  color: Colors.lightBlue[100],
+                ),
+              ),
+            ],
+          ),
+          centerTitle: false,
+          backgroundColor: Colors.lightBlue[800],
+        );
+    }
+}
 
 
 class MessageMenu extends StatelessWidget {
@@ -16,7 +60,7 @@ class MessageMenu extends StatelessWidget {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50),
-        child: CustomAppBar(),
+        child: MessagesAppBar(),
         ),
         
       drawer: DrawerContent(),  
