7bb45296f1ec855d58d0348647ec922bd524d9a4
jensen-lloyd <jl.110@protonmail.com>
2023-12-10 16:43:19 +1100

    deleted settingsPage, created structure for settings menu and homepage

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -56,9 +56,93 @@ class HomePage extends StatelessWidget {
       drawer: AppDrawer(),  
         
         
-      body: Text('Home page content'),
+      body: HomePageContent(),
 
 
     );
   }
+}
+
+
+class HomePageContent extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Column(
+            children: [
+                Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(225, 225, 225, 1.0),
+                        border: Border(
+                            bottom: BorderSide(
+                                width: 3,
+                                color: Color.fromRGBO(110, 110, 120, 1.0),
+                            ),
+                        ),
+                    ),
+                    
+                    
+                    child: Row(
+                    
+                    
+                    
+                    
+                    
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                        
+                        //Section icon
+                            Container(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                margin: EdgeInsets.fromLTRB(10, 11, 5, 11),
+                                //child: Icon(Icons.home),
+                            ),
+                            
+                            Column(
+                                children: [
+                                    //Section name
+                                    Container(
+                                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                        width: (MediaQuery.of(context).size.width) * 0.9,
+                                        child: Text(
+                                            "About X",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 20,
+                                                color: Colors.black,
+                                            ),
+                                            softWrap: true,
+                                            overflow: TextOverflow.ellipsis,
+                                        ),
+                                    ),
+                                    
+                                    //Section description/info
+                                    Container(
+                                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                        width: (MediaQuery.of(context).size.width) * 0.9,
+                                        //height: 40,
+                                        child: Text(
+                                            "\nX is a multi-function cross-platform app that offers a variety of features to enable users to take back control of their digital privacy. \n\nX was built with home-labbers in mind, to provide a single point for a large number of daily services to be brought out of the cloud, back to your own hardware, under your control. \n\nOur mission is to enable you to easily setup and access as many services as possible from a single point on your server, accessible by a single app from your phone, tablet, laptop, and desktop.",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                           ),
+                                            softWrap: true,
+                                            overflow: TextOverflow.clip,
+                                        ),
+                                    ),
+                                ],
+                            ),
+                            
+                    	],
+                            
+                            
+                            
+                    )
+                )
+            ]
+        );
+    }
 }
--- lib/SettingsMenu.dart
+++ lib/SettingsMenu.dart
@@ -55,9 +55,98 @@ class SettingsMenu extends StatelessWidget {
       drawer: AppDrawer(),  
         
         
-      body: Text('Settings menu'),
+      body: SettingsPageContent(),
 
 
     );
   }
+}
+
+
+class SettingsPageContent extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Column(
+            children: [
+                Container(
+                    //height: 80,
+                    decoration: BoxDecoration(
+                        color: Color.fromRGBO(225, 225, 225, 1.0),
+                        border: Border(
+                            bottom: BorderSide(
+                                width: 3,
+                                color: Color.fromRGBO(110, 110, 120, 1.0),
+                            ),
+                        ),
+                    ),
+                        
+                        
+                    child: Row(
+                    
+                    
+                    
+                    
+                    
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                        
+                            Container(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                margin: EdgeInsets.fromLTRB(10, 11, 5, 11),
+                                child: Icon(Icons.home),
+                            ),
+                            
+                            Column(
+                                children: [
+                                    //Setting name
+                                    Container(
+                                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                        width: (MediaQuery.of(context).size.width) * 0.5,
+                                        child: Text(
+                                            "Theme",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w500,
+                                                fontSize: 20,
+                                                color: Colors.black,
+                                            ),
+                                            softWrap: true,
+                                            overflow: TextOverflow.ellipsis,
+                                        ),
+                                    ),
+                                    
+                                    //setting description
+                                    Container(
+                                        padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                        width: (MediaQuery.of(context).size.width) * 0.5,
+                                        //height: 40,
+                                        child: Text(
+                                            "Enable or disable Dark mode (default is Light mode)",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w400,
+                                                fontSize: 15,
+                                                color: Colors.black,
+                                           ),
+                                            softWrap: true,
+                                            overflow: TextOverflow.clip,
+                                        ),
+                                    ),
+                                ],
+                            ),
+                            Container(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                                margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                                child: Icon(Icons.toggle_off),
+                                    
+                        	)	
+                    	],
+                            
+                            
+                            
+                    )
+                )
+            ]
+        );
+    }
 }
